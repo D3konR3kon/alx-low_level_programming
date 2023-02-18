4f82769dd28491d0cf2ff75994845e57311486bc
Vuyo Dila <dilavuyo@outlook.com>
2023-02-17 18:21:08 -0800

    double combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int i;
+	int x;
+
+	for (x  = 48; x <= 57; x++)
+	{
+		for (i = x + 1; i <= 57; i++)
+		{
+			putchar(x);
+			putchar(i);
+
+			if (i != 56 || x != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
