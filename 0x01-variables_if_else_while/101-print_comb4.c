bf6488f6df15139ea5577183d8f79bb3c1c77fd4
Vuyo Dila <dilavuyo@outlook.com>
2023-02-17 18:42:06 -0800

    triple digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int i;
+	int x;
+	int n;
+
+	for (x  = 48; x < 56; x++)
+	{
+		for (i = x + 1; i < 57; i++)
+		{
+			for (n = i + 1; n < 58; n++)
+			{
+				putchar(x);
+				putchar(i);
+				putchar(n)
+				if (x != 55 || i != 56 || n != 57)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
